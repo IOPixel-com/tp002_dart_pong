d9148e2ee181434b111951b64a5d482f067a7c8d
ellis2323 <laurent.mallet@gmail.com>
2020-02-27 14:47:53 +0100

    init project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+
+void main() => runApp(Starfield().widget);
+
+class Starfield extends Game {
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
+}
