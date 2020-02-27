44951de5c95e9c07b8bc8828dfe53904a68bb8f4
ellis2323 <laurent.mallet@gmail.com>
2020-02-27 15:10:39 +0100

    dessine un point

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
 void main() => runApp(Starfield().widget);
 
 class Starfield extends Game {
+  Size _screenSize = Size(0,0);
+
+  @override
+  void resize(Size sz) {
+    // fonction appelee quand la taille de l ecran est definie
+    _screenSize = sz;
+  }
+
   @override
   void render(Canvas canvas) {
-    // TODO: implement render
+    var points = List<Offset>(1);
+    points[0] = _screenSize.center(Offset(0, 0));
+    // definit les caracteristiques graphiques du point (couleur et largeur)
+    Paint paint = Paint();
+    paint.strokeWidth = 50;
+    paint.color = Colors.grey;
+    // dessine un point au centre de l ecran
+    canvas.drawPoints(PointMode.points, points, paint);
   }
 
   @override
   void update(double t) {
-    // TODO: implement update
+    // delta en second entre deux images
+    // a 60 fps (ips en fr) cela correspond a 0.0166s
+    // si on a 0.032ms, on a un traitement graphique trop lourd pour une image
+    print('delta time in ms between two frames: $t');
   }
 }
