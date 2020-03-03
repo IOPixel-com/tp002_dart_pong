fd1299b67ef0eb3084a30beb1676a7b001c08598
ellis2323 <laurent.mallet@gmail.com>
2020-03-03 21:37:17 +0100

    add images

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,83 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame/position.dart';
+import 'package:flame/flame.dart';
 
-void main() => runApp(Starfield().widget);
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  runApp(Pong().widget);
+}
 
-class Starfield extends Game {
-  Size _screenSize = Size(0,0);
+class IOSprite {
+  Sprite _sprite;
+  Position _pos;
+  Position _size;
+
+  IOSprite.upperLeft(String filename, this._pos, this._size)
+      : _sprite = Sprite(filename);
+
+  void resize(Position sz) {
+    _size = sz;
+  }
+
+  void draw(Canvas canvas) {
+    _sprite.renderPosition(canvas, _pos, size: _size);
+  }
+}
+
+class IOScene {
+  IOSprite _bg;
+  IOSprite _player;
+  IOSprite _computer;
+
+  IOScene(Position windowSize) {
+    _bg = IOSprite.upperLeft('bg_metal.png', Position(0, 0), windowSize);
+    _player = IOSprite.upperLeft('puck.png', Position(0, 0), Position(64, 64));
+    _computer = IOSprite.upperLeft(
+        'puck.png', Position(0, windowSize.y - 64), Position(64, 64));
+        _
+  }
+
+  void resize(Position size) {
+    _bg.resize(size);
+  }
+
+  void draw(Canvas canvas) {
+    _bg.draw(canvas);
+    _player.draw(canvas);
+    _computer.draw(canvas);
+  }
+}
+
+class Pong extends Game {
+  double _date = 0;
+  IOScene _scene;
+
+  Pong() {
+    //_player = Sprite('sprite.png');
+    print('constructor');
+  }
 
   @override
   void resize(Size sz) {
     // fonction appelee quand la taille de l ecran est definie
-    _screenSize = sz;
+    print('resize $sz');
+    if (_scene == null) {
+      _scene = IOScene(Position(sz.width, sz.height));
+      Flame.util.fullScreen();
+      Flame.util.setPortrait();
+    } else {
+      _scene.resize(Position(sz.width, sz.height));
+    }
   }
 
   @override
   void render(Canvas canvas) {
-    var points = List<Offset>(1);
-    points[0] = _screenSize.center(Offset(0, 0));
-    // definit les caracteristiques graphiques du point (couleur et largeur)
-    Paint paint = Paint();
-    paint.strokeWidth = 50;
-    paint.color = Colors.grey;
-    // dessine un point au centre de l ecran
-    canvas.drawPoints(PointMode.points, points, paint);
+    if (_scene != null) {
+      _scene.draw(canvas);
+    }
   }
 
   @override
@@ -31,6 +86,7 @@ class Starfield extends Game {
     // delta en second entre deux images
     // a 60 fps (ips en fr) cela correspond a 0.0166s
     // si on a 0.032ms, on a un traitement graphique trop lourd pour une image
-    print('delta time in ms between two frames: $t');
+    print('delta time in ms between two frames: $_date $t');
+    _date += t;
   }
 }
